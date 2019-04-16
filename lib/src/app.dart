40079efda50bbcd55d471570c5c670c77e40820c
David Sanchez <deividsanchez96@hotmail.com>
2019-04-15 22:37:10 -0500

    App completed

---
+++ lib/src/app.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
+import 'widgets/image_list.dart';
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 1;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    var response =
+        await get("https://jsonplaceholder.typicode.com/photos/$counter");
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+      counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Let's See Images!"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: fetchImage,
+          child: Icon(Icons.add),
+        ),
+        body: ImageList(images),
+      ),
+    );
+  }
+}
